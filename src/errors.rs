db26d4172dff6d2b9fa93d3bdc609b52e5a79487
Adán SDPC <adan.sdpc@gmail.com>
2026-03-18 16:30:00 +0100

    feat: add basic support for error handling

---
+++ src/errors.rs
@@ -0,0 +1,21 @@
+use crate::types::*;
+#[derive(thiserror::Error, Debug, PartialEq)]
+pub enum Error {
+    #[error("tried to process a deposit without specifying an amount")]
+    DepositWithoutAmount,
+    #[error("tried to process a transaction for an account that is locked")]
+    LockedAccount,
+    #[error("unknown or indeterminate error: {_0}")]
+    UnknownError(String),
+    #[error(
+        "tried to withdraw more value ({withdrawing}) than the current available balance ({available})"
+    )]
+    WithdrawalAmountExceedsAvailableBalance {
+        withdrawing: Value,
+        available: Value,
+    },
+    #[error("tried to process a withdrawal without specifying an amount")]
+    WithdrawalWithoutAmount,
+    #[error("cannot process transactions with a zero or negative amount ({_0})")]
+    ZeroOrNegativeAmount(Value),
+}
