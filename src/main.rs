1bf9da7dc92bbc1c05ddeb5b15992a277f25d5b7
Adán SDPC <adan.sdpc@gmail.com>
2026-03-18 10:55:10 +0100

    chore: add basic modules structure for the crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+/// Everything related to user accounts, balances, etc.
+pub mod accounts;
+/// The main data structure that will act as the core of our runtime and will implement the business
+/// logic of transaction processing.
+pub mod engine;
+/// Everything related with transactions and their semantics.
+pub mod transactions;
+
 fn main() {
     println!("Hello, world!");
 }
